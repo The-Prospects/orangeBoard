58662a4c41be5854d3d18d94c4c835a2484a54be
Sean Park <chanseanpark@gmail.com>
2020-12-17 21:45:23 -0800

    changed background color

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 //heroku features:enable http-session-affinity
 //to work with socket io
 
-function onConnection(socket){
-  socket.on('drawing', function(data){
+function onConnection(socket) {
+  socket.on('drawing', function (data) {
     socket.broadcast.emit('drawing', data);
     console.log(data);
   });
-  
-  socket.on('rectangle', function(data){
+
+  socket.on('rectangle', function (data) {
     socket.broadcast.emit('rectangle', data);
     console.log(data);
   });
-  
-  socket.on('linedraw', function(data){
+
+  socket.on('linedraw', function (data) {
     socket.broadcast.emit('linedraw', data);
     console.log(data);
   });
-  
-   socket.on('circledraw', function(data){
+
+  socket.on('circledraw', function (data) {
     socket.broadcast.emit('circledraw', data);
     console.log(data);
   });
-  
-  socket.on('ellipsedraw', function(data){
+
+  socket.on('ellipsedraw', function (data) {
     socket.broadcast.emit('ellipsedraw', data);
     console.log(data);
   });
-  
-  socket.on('textdraw', function(data){
+
+  socket.on('textdraw', function (data) {
     socket.broadcast.emit('textdraw', data);
     console.log(data);
   });
-  
-  socket.on('copyCanvas', function(data){
+
+  socket.on('copyCanvas', function (data) {
     socket.broadcast.emit('copyCanvas', data);
     console.log(data);
   });
-  
-  socket.on('Clearboard', function(data){
+
+  socket.on('Clearboard', function (data) {
     socket.broadcast.emit('Clearboard', data);
     console.log(data);
   });
- 
 }
 
 io.on('connection', onConnection);
