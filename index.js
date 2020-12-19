e53b976d6d7bbeae1adf7878ec90fc5f6ccc79ce
Tristan Harville <71527520+OSUTristanH@users.noreply.github.com>
2020-12-19 03:57:58 -0600

    Start branch

--- index.js
+++ index.js
@@ -4,6 +4,39 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+require('dotenv').config();
+const { App } = require('@slack/bolt');
+
+const bot = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+(async () => {
+  // Start the app
+  await bot.start(process.env.PORT || 4000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
+
+http.listen(port, () => console.log('listening on port ' + port));
+
+// Gives the response
+bot.event("app_mention", async ({ context, event }) => {
+
+  try{
+    await bot.client.chat.postMessage({
+      token: context.botToken,
+      channel: event.channel,
+      text: `Hello <@${event.user}> you mentioned me. Here is the link for your Orange Board:`
+    });
+  }
+  catch (e) {
+    console.log(`error responding ${e}`);
+  }
+
+});
+
 app.use(express.static(__dirname + '/public'));
 
 function onConnection(socket) {
@@ -50,4 +83,4 @@ function onConnection(socket) {
 
 io.on('connection', onConnection);
 
-http.listen(port, () => console.log('listening on port ' + port));
+
