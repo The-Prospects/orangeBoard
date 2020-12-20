4fad6f44ccc52b78ddace25f37220b59eb2106f9
Tristan Harville <71527520+OSUTristanH@users.noreply.github.com>
2020-12-20 02:03:35 -0600

    Added botapi & room routes.

--- routes/botapi.js
+++ routes/botapi.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(function(req, res, next){
+   req.extension = "dude";
+   next();
+});
+
 router.get('/', (req, res) =>{
-    res.json({url: 'https://orange-board.herokuapp.com/'});
+    res.json({url: 'https://orange-board.herokuapp.com/' + req.extension});
 });
 
 module.exports = router;
